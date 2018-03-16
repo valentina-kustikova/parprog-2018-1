53acb2acf4d36bd1d7db4dad9a9633d944242048
TvoiSempai <lumpyourhead1@gmail.com>
2018-03-16 12:08:59 +0300

    filtruet

--- groups/1506-1/mishutin_as/1-test-version/TestVersion/TestVersion/SeqFilter.cpp
+++ groups/1506-1/mishutin_as/1-test-version/TestVersion/TestVersion/SeqFilter.cpp
@@ -14,8 +14,10 @@ for (int i = -radius; i <= radius; ++i) {
 }
 
 //sequential gaussian filter
-void seqGaussFilter(Mat& source, Mat& output, int radius, double sigma) {
+void seqGaussFilter(Mat& source, Mat& output1, int radius, double sigma) {
 	double **kernel;
+	//output is a temporary matrix
+	Mat output = Mat::zeros(source.rows + 2, source.cols + 2, CV_8UC3);
 	kernel = new double*[radius * 2 + 1];
 	for (int i = 0; i < radius * 2 + 1; ++i) kernel[i] = new double[radius * 2 + 1];
 	fillKernel(kernel, radius, sigma);
@@ -78,17 +80,52 @@ void seqGaussFilter(Mat& source, Mat& output, int radius, double sigma) {
 	//work with inner image
 	for (int x = 1; x < output.rows - 1; ++x) {
 		for (int y = 1; y < output.cols - 1; ++y) {
-			double rSum = 0, gSum = 0, bSum = 0;
+			double rSum = 0, gSum = 0, bSum = 0, kSum = 0;
 			//work with kernel
 			for (int i = 0; i < radius * 2 + 1; ++i) {
 				for (int j = 0; j < radius * 2 + 1; ++j) {
 					int posX = x + ( i - (radius * 2 + 1) / 2);
 					int posY = y + (j - (radius * 2 + 1) / 2);
+					int r = output.at<Vec3b>(posX, posY)[0];
+					int g = output.at<Vec3b>(posX, posY)[1];
+					int b = output.at<Vec3b>(posX, posY)[2];
+					double kernelVal = kernel[i][j];
+					rSum += r * kernelVal;
+					gSum += g * kernelVal;
+					bSum += b * kernelVal;
+
+					kSum += kernelVal;
 
 				}
 			}
+			if (kSum <= 0) kSum = 1;
+
+			// онтролируем переполнени€ переменных
+			rSum /= kSum;
+			if (rSum < 0) rSum = 0;
+			if (rSum > 255) rSum = 255;
+
+			gSum /= kSum;
+			if (gSum < 0) gSum = 0;
+			if (gSum > 255) gSum = 255;
+
+			bSum /= kSum;
+			if (bSum < 0) bSum = 0;
+			if (bSum > 255) bSum = 255;
+			output.at<Vec3b>(x,y)[0] = rSum;
+			output.at<Vec3b>(x, y)[1] = gSum;
+			output.at<Vec3b>(x, y)[2] = bSum;
 
 		}
+	}	
+	//copying into the final image
+	for (int i = 1; i < output.rows -1; ++i) {
+		for (int j = 1; j < output.cols -1; ++j) {
+			output1.at<Vec3b>(i - 1, j - 1)[0] = output.at<Vec3b>(i, j)[0];
+			output1.at<Vec3b>(i - 1, j - 1)[1] = output.at<Vec3b>(i, j)[1];
+			output1.at<Vec3b>(i - 1, j - 1)[2] = output.at<Vec3b>(i, j)[2];
+		}
 	}
+
 	delete(kernel);
 }
